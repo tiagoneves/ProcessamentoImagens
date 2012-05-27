d182cf18725e818d529e2ff05753593a1f7e00c1
Tiago Neves <tiagodvneves@yahoo.com.br>
2012-05-27 18:54:51 -0300

    commit

--- ProcessamentoImagens/build.sh
+++ ProcessamentoImagens/build.sh
@@ -1,1 +1,1 @@
-g++ -o main classes/*.cpp main.cpp `pkg-config opencv --cflags --libs` -I./include
+g++ -o main classes/excecoes.cpp classes/imagem.cpp classes/imagem_opencv.cpp main.cpp `pkg-config opencv --cflags --libs` -I./include
