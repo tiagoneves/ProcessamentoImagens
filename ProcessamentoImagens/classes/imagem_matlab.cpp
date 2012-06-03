d2cef41729ec0b9e9e8692cf284ea526a6886950
Tiago Neves <tiagodvneves@yahoo.com.br>
2012-06-03 18:17:09 -0300

    commit

---
+++ ProcessamentoImagens/classes/imagem_matlab.cpp
@@ -0,0 +1,40 @@
+
+#include "../include/imagem_matlab.h"
+#include "../include/excecoes.h"
+
+
+ImagemOpenCV::ImagemOpenCV(const char* caminhoEntrada)  : Imagem (caminhoEntrada){
+
+    //TODO
+
+}
+
+void ImagemOpenCV::redimensionar(double width, double height) const{
+
+    //TODO
+}
+
+void ImagemOpenCV::converter(const string output) const{
+
+   //TODO
+
+}
+
+void ImagemOpenCV::crop(double cx, double cy, int width, int height)const{
+
+   //TODO
+
+}
+
+void ImagemOpenCV::rotacionar(double cx, double cy, double angulo) const{
+
+   //TODO
+
+
+}
+
+void ImagemOpenCV::salvar(const char* localSaida) const {
+
+    //TODO
+
+}
